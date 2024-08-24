a240a731f576f07da70346af5d9658df553107bc
Adnan Semic <148137889+ShemichAdnan@users.noreply.github.com>
2024-08-24 13:47:52 +0200

    ZAvrsen projekat

--- javascript.js
+++ javascript.js
@@ -7,7 +7,7 @@ const generate=document.querySelector("#generate")
 let game=()=>{
     container.innerHTML='';
     const number=document.querySelector("#input");
-
+    
     let size=number.value;
     if(size>100){
         alert("Your number is too high!")
@@ -21,13 +21,16 @@ let game=()=>{
             
             for(let j=0;j<size;j++){
                 const squares=document.createElement("div");
-                squares.style.backgroundColor="white";
+                squares.style.backgroundColor="black";
+                squares.style.opacity=0.1;
                 squares.style.color="white";
                 squares.setAttribute("class","square")
                 squares.style.flex="1";
                 squares.addEventListener("mouseover",()=>{
-                    squares.style.backgroundColor="black";
-                    console.log("boja")
+                    let opacity=squares.style.opacity;
+                    opacity=+opacity+0.1;
+                    squares.style.opacity=opacity;
+                    
                 });
                 row.appendChild(squares);
             }
